2d4a88b2fd9e1467f389276c596da1a96e8cf95c
Brian Zelip <bzelip@gmail.com>
2014-05-15 23:42:01 -0500

    fleshed out idea

---
+++ catalog.php
@@ -0,0 +1,174 @@
+<!--begin main content-->
+  <div class="hero-unit">
+    <div class="container">
+      <h2>Catalog</h2>
+    </div>
+    <!--close container--> 
+  </div>
+
+  <!--close hero-unit-->
+  <div class="container clearfix" id="main-content"> 
+
+    <!--begin main content-->
+
+    <p>Minrva 2.0.2 for Android works with all I-Share libraries in Illinois. That means if your library is part of I-Share, Minrva is already available and free for your patrons. You can start using Minrva today at your I-Share library by simply downloading the app, and then choosing your library when prompted for a location.</p>
+    <h3 class="short_headline"><span>Minrva Core</span></h3>
+    <p>The Minrva core are the set of modules available for all I-Share libraries in CARLI.</p>
+    <!-- begin equalHeights columns 4 -->
+    <div id="equalHeightsB" class="row-fluid equalHero">
+      <div class="span3">
+        <h3><a href="modules-blocks">Blocks</a></h3>
+        <p><a href="modules-blocks"><img src="public/themes/minrva/assets/images/blocks/blocks.png" class="alignleft" alt="A book with a red x through it." /></a>
+        Displays a list of blocks against a user account. Information includes: the institution that assigned the block, the day and time that the 
+        block was instituted, and the description of the block.</p>
+        <p class="right"><a href="modules-blocks" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+      <div class="span3">
+        <h3><a href="modules-catalog">Catalog Search</a></h3>
+        <p><a href="modules-catalog"><img src="public/themes/minrva/assets/images/catalog/catalog.png" class="alignleft" alt="A magnifying glass."></a>An interface to the library catalog. Students are able to refine searches by location, type of search, and free text input. 
+          The module is able to display many pages of search results as a user scrolls. </p>
+        <p class="right"><a href="modules-catalog" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+      <div class="span3">
+        <h3><a href="modules-checked-out">Checked Out</a></h3>
+        <p><a href="modules-checked-out"><img src="public/themes/minrva/assets/images/checked_out/checked_out.png" class="alignleft" alt="A stack of books with a checkmark written over them."></a>Displays a list of checked out items. Information includes: the title, institution, due date, and current status ("Checked-out" or "Renewed") of the borrowed item. Users can renew any of their currently checked-out items.</p>
+        <p class="right"><a href="modules-checked-out" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3-->
+      <div class="span3">
+        <h3><a href="modules-cite">Citation</a></h3>
+        <p><a href="modules-cite"><img src="public/themes/minrva/assets/images/cite/cite.png" class="alignleft" alt="A paper labeled Citations with cogs drawn on top of the paper."></a>The citation module is designed to allow a user to quickly and accurately cite a wide variety of sources in either MLA or APA format. The module also allows the user to email the completed citation.</p>
+        <p class="right"><a href="modules-cite" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+    </div>
+    <!-- close #equalHeights .featured 4 columns --> 
+    <!-- begin equalHeights columns 4 -->
+    <div id="equalHeightsB" class="row-fluid equalHero">
+      <div class="span3">
+        <h3><a href="modules-reserves">Course Reserves</a></h3>
+        <p><a href="modules-reserves"><img src="public/themes/minrva/assets/images/reserves/reserves.png" class="alignleft" alt="A stack of books labeled 'Reserved'."></a>Allows you to quickly view all reserves for a particular course, including print reserve materials, electronic reserves, and any media items. Simply search by course, instructor, or department.</p>
+        <p class="right"><a href="modules-reserves" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 --> 
+      <div class="span3">
+        <h3><a href="modules-favorites">Favorites</a></h3>
+        <p><a href="modules-favorites"><img src="public/themes/minrva/assets/images/favorites/favorites.png" class="alignleft" alt="A stack of books with a hand gesturing thumbs up on top of them."></a>Displays a list of favorited items. Information displayed in the favorites module includes: the title of the favorited item along with the author(s), artist(s), subtitles, etc.</p>
+        <p class="right"><a href="modules-favorites" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3-->
+      <div class="span3">
+        <h3><a href="modules-fines">Fines</a></h3>
+        <p><a href="modules-fines"><img src="public/themes/minrva/assets/images/fines/fines.png" class="alignleft" alt="A pile of gold coins."></a>Displays a list of all fines associated with this account. Information displayed in the fines module includes: title of item, date fine was levied, amount, and type of fine.</p>
+        <p class="right"><a href="modules-fines" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 --> 
+      <div class="span3">
+        <h3><a href="modules-requested-items">Requested Items</a></h3>
+        <p><a href="modules-requested-items"><img src="public/themes/minrva/assets/images/requested/requested.png" class="alignleft" alt="A basket with a stack of books in it."></a>Displays a list of all of the items that a user has requested. This module also allows the user to cancel any requests that they have placed.</p>
+        <p class="right"><a href="modules-requested-items" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+    </div>
+    <!-- close #equalHeights .featured 4 columns --> 
+
+  <h3 class="short_headline"><span>Minrva Toolbar</span></h3>
+  <!-- begin equalHeights columns 3 -->
+  <div id="equalHeightsB" class="row-fluid equalHero">
+    <div class="span5">
+        <img src="public/themes/minrva/assets/images/toolbar/minrva_toolbar.png" class="alignleft" alt="An image of the toolbar portion of the app.">
+    </div>
+    <div class="span4">
+      <br>
+      <img src="public/themes/minrva/assets/images/toolbar/A.png" class="alignleft" alt="An image of the letter 'A'">
+       <p>The "Recent Stack" displays a list of items that were recently searched and selected.</p>
+      <p class="right"><a href="modules-recent-stack" class="btn btn-primary">Details &rarr;</a></p>
+    </div>
+    <!-- close .span3 -->
+    <div class="span3">
+      <br>
+      <img src="public/themes/minrva/assets/images/toolbar/B.png" class="alignleft" alt="An image of the letter 'B'">
+      <p>Change your location. The location you select will be the search target for the catalog and will populate available modules.</p>
+      <br>
+      <img src="public/themes/minrva/assets/images/toolbar/C.png" class="alignleft" alt="An image of the letter 'C'">  
+      <p>Learn more about the selected module by tapping the information icon in the top right of the toolbar.</p>
+    </div>
+    <!-- close .span3 -->
+  <!-- close equalHeights featured 3 columns -->
+  </div>
+
+  <h3 class="short_headline"><span>Minrva Custom</span></h3>
+    <p>The Minrva custom modules are the set of modules that have custom functionality and databases configured for select unit libraries of the University of Illinois at Urbana-Champaign campus. If you would like to add custom modules for your library please <a href="http://minrvaproject.org/contact">contact us</a>.</p>
+    <!-- begin equalHeights columns 4 -->
+    <div id="equalHeightsB" class="row-fluid equalHero">
+      <div class="span3">
+        <h3><a href="modules-journal-search">Journal Search</a></h3>
+        <p><a href="modules-journal-search"><img src="public/themes/minrva/assets/images/journals/journals.png" class="alignleft" alt="A journal book with a magnifying glass hovering over it."></a>The journal search module acts as an interface to journal database providers. Users are authenticated to ensure that the
+          students have access to the database of interest.</p>
+        <p class="right"><a href="modules-journal-search" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+      <div class="span3">
+        <h3><a href="modules-qb">Question Board</a></h3>
+        <p><a href="modules-qb"><img src="public/themes/minrva/assets/images/qb/qb.png" class="alignleft" alt="A basket with a stack of books in it."></a>The Question Board module displays a list of questions and answers. The Question Board is an anonymous question and answer service provided by the Undergraduate Library at the University of Illinois.</p>
+        <p class="right"><a href="modules-qb" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+      <div class="span3">
+        <h3><a href="modules-scanner">Scanner</a></h3>
+        <p><a href="modules-scanner"><img src="public/themes/minrva/assets/images/scanner/scanner.png" class="alignleft" alt="A barcode being scanned by a red light."></a>The scanner module allows a user to easily scan a book for use by other modules. The scanner supports barcodes with Codabar format.</p>
+        <p class="right"><a href="modules-scanner" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+      <div class="span3">
+        <h3><a href="modules-technology">Technology</a></h3>
+        <p><a href="modules-technology"><img src="public/themes/minrva/assets/images/tech/tech.png" class="alignleft" alt="A laptop with a camera sitting on the keyboard."></a>The technology module displays a list of loanable technology items along with a count of how how many are currently available.</p>
+        <p class="right"><a href="modules-technology" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3-->
+    <!-- close #equalHeights .featured 4 columns --> 
+    </div>
+    <!-- begin equalHeights columns 4 -->
+    <div id="equalHeightsB" class="row-fluid equalHero">
+      <div class="span3">
+        <h3><a href="modules-topic-space">Topic Space (coming soon!)</a></h3>
+        <p><a href="modules-topic-space"><img src="public/themes/minrva/assets/images/topicspace/topic_space.png" class="alignleft" alt="A rendering of the library topic spaces."></a>Topic space allows a user to take a picture of an item's call number in the book stacks. The module will show the user other books that are relevant but that are not shelved nearby; it can also show users books that are normally shelved here but that are currently checked out.</p>
+        <p class="right"><a href="modules-topic-space" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3-->
+      <div class="span3">
+        <h3><a href="modules-wayfinder">Wayfinder</a></h3>
+        <p><a href="modules-wayfinder"><img src="public/themes/minrva/assets/images/wayfinder/wayfinder.png" class="alignleft" alt="A blank map with a dotted line path leading up to a star."></a>Displays a conceptual map of the library drawn to scale along with a red dot symbolizing where the currently selected book is located.</p>
+        <p class="right"><a href="modules-wayfinder" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+    </div>
+    <!-- close #equalHeights .featured 4 columns --> 
+  <h3 class="short_headline"><span>Coming Soon!</span></h3>
+    <p>Modules that we plan to release in future Minrva versions.</p>
+    <!-- begin equalHeights columns 4 -->
+    <div id="equalHeightsB" class="row-fluid equalHero">
+      <div class="span3">
+        <h3><a href="modules-text-shot">Text-shot (coming soon!)</a></h3>
+        <p><a href="modules-text-shot"><img src="public/themes/minrva/assets/images/textshot/text_shot.png" class="alignleft" alt="An assignment sheet being scanned."></a>Text-shot allows a user to take a picture of their assignment sheet or syllabus to get recommendations for library guides (LibGuides), library content (articles or books), or related subjects terms to search.</p>
+        <p class="right"><a href="modules-text-shot" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+      <div class="span3">
+        <h3><a href="modules-room-reserve">Room Reserve (coming soon!)</a></h3>
+        <p><a href="modules-room-reserve"><img src="public/themes/minrva/assets/images/roomreserve/room_reservation.png" class="alignleft" alt="A room with students inside."></a>Allows a student to reserve a study room in the library.</p>
+        <p class="right"><a href="modules-room-reserve" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 -->
+      <div class="span3">
+        <h3><a href="modules-new-books">New Books (coming soon!)</a></h3>
+        <p><a href="modules-new-books"><img src="public/themes/minrva/assets/images/newbooks/newbooks.png" class="alignleft" alt="A new book image."></a>Shows students recently added items to the library location selected.</p>
+        <p class="right"><a href="modules-new-books" class="btn btn-primary">Details &rarr;</a></p>
+      </div>
+      <!-- close .span3 --> 
+    </div>
+    <!-- close #equalHeights .featured 4 columns -->
+  </div>
+  <!--close .container id="main-content" -->
